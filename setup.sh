732f213305780afcdaed0a9baec1f1a01bb1632e
Psabo-LUC <Psaboluc@gmail.com>
2026-06-04 11:31:47 -0500

    Added setup.sh file to the repository. This file contains instructions for setting up the environment and installing necessary dependencies for the project. The setup.sh script will help users quickly get started with the project by automating the installation process.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-venv
+python3 -m venv ./venv
+source ./venv/bin/activate 
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
